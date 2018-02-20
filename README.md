c70144784e1f93c6e00f1db1f71ec72e924f29ff
ltaupiac <ltaupiac@lfdj.com>
2018-02-20 11:00:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RestartPC
+Restart the User device
+An alert message is displayed to user
---
+++ RestartDevice.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.SYNOPSIS
+Nexthink ACT: Restart device
+.DESCRIPTION
+Restart Device
+
+.FUNCTIONALITY
+On demand
+
+.PARAMETERS Message
+Message shown to user when reboot action is run
+
+.LINK
+https://github.com/ltaupiac/restartDevice
+https://www.nexthink.com
+https://www.nexthink.com/act-release/
+
+.NOTES
+Context: User
+Version 1.0.0.0
+#>
+
+param(
+    [Parameter(Mandatory=$true)][string]$Message
+)
+
+
+try {
+    shutdown.exe /r /c $Message
+    exit 0
+ }
+catch {
+    Write-Error "Can't reboot device" + $_.Exception.Message
+    exit 1
+}
